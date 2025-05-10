567e1584edce4de132cc6dd15df368df981db316
AndersonOsbeli <andersonosbelitt555@gmail.com>
2025-05-10 12:54:00 -0600

    agregando archivo registroUsuario.java

---
+++ registroUsuario/src/main/java/com/mycompany/registrousuario/RegistroUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrousuario;
+
+/**
+ *
+ * @author osbel
+ */
+public class RegistroUsuario {
+
+    public static void main(String[] args) {
+        System.out.println("registro usuario en java");
+    }
+}
